7eed6f150be02d97ce8bb01727e16946ae1c0830
insomniac206 <harishabhinav027@gmail.com>
2024-05-31 20:20:53 +0530

    added descriptions, new mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,20 @@
 -- Normal mode mappings
-vim.keymap.set('n', '<Leader>e', ':NvimTreeToggle<CR>')
-vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('n', '<Leader>e', ':NvimTreeToggle<CR>', {desc = "Toggle Sidebar (file explorer)"})
+vim.keymap.set('n', '<C-s>', ':w<CR>', {desc = "Save File"})
+vim.keymap.set('n', '<Leader>p', ':Lazy<CR>', {desc = "Open Plugin Settings/List"})
+vim.keymap.set('n', '<Leader>q', ':q!<CR>', {desc = "Quit Neovim"})
+vim.keymap.set('n', 'T', ':bn<CR>', {desc = "Next Tab"})
+vim.keymap.set('n', 'C', ':bd<CR>', {desc = "Close Tab"})
+
+-- Insert mode mappings
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>i')
+vim.keymap.set('i', '<C-x>', '<Esc>')
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find files in the current working directory"})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "grep"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Go to file"})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "Telescope help"})
 
 
