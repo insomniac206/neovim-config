7ded66170ee882022c75d309b3af3771b49c73b3
insomniac206 <harishabhinav027@gmail.com>
2024-05-31 20:21:49 +0530

    Added more plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,5 +4,24 @@ return {
     "nvim-tree/nvim-tree.lua",
     {"nvim-telescope/telescope.nvim", tag="0.1.6", dependencies={"nvim-lua/plenary.nvim"}},
     {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-    {"nvim-lualine/lualine.nvim", dependencies = "nvim-tree/nvim-web-devicons"}
+    {"nvim-lualine/lualine.nvim", dependencies = "nvim-tree/nvim-web-devicons"},
+    {
+        "goolord/alpha-nvim",
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+            'nvim-lua/plenary.nvim'
+        },
+        config = function()
+            require('alpha').setup(require('alpha.themes.dashboard').config)
+        end
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {}
+    },
 }
