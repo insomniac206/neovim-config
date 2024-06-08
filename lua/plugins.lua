0050e1cec5d7dca1af501e8d9b8ca6f1f1f12e30
insomniac206 <harishabhinav027@gmail.com>
2024-06-08 12:42:53 +0530

    added treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,4 +27,19 @@ return {
         end,
         opts = {}
     },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = {"c", "cpp", "python", "lua", "json", "javascript", "html", "css"},
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true }
+            })
+        end
+    }
 }
